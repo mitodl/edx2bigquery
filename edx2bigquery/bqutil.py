7de1520a53d73ed93a993770b29a75f98cf4b8a0
Glenn Lopez <glenn_lopez@harvard.edu>
2014-12-04 16:57:01 -0500

    make sure to get all rows from table data
    When the table data retrieved is less than total rows in table, then
    execute table data call again and append the remaining data

--- edx2bigquery/bqutil.py
+++ edx2bigquery/bqutil.py
@@ -131,6 +131,14 @@ def get_table_data(dataset_id, table_id, key=None, logger=None, project_id=DEFAU
         table_ref['startIndex'] = startIndex
 
     data = tabledata.list(**table_ref).execute()
+    dataRows = int(len(data['rows']))
+    totalRows = int(data['totalRows'])
+    while (dataRows < totalRows):
+       table_ref['startIndex'] = dataRows
+       data_append = tabledata.list(**table_ref).execute()
+       data['rows'] += data_append['rows']
+       dataRows = int(len(data['rows']))
+    print "[bqutil] Total Rows Retrieved: %s" % dataRows
 
     fields = table['schema']['fields']
     field_names = [x['name'] for x in fields]
