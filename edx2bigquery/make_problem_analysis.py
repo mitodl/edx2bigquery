227737507640d934d0e87c200b0827253bd6fd30
Daniel Seaton <dseaton@gmail.com>
2015-08-27 06:43:25 +0000

    debugging

--- edx2bigquery/make_problem_analysis.py
+++ edx2bigquery/make_problem_analysis.py
@@ -135,7 +135,13 @@ def make_problem_analysis(course_id, basedir=None, datedir=None, force_recompute
         # May 2015: new location syntax for module_id's, e.g.:
         # block-v1:HarvardX+BUS5.1x+3T2015+type@sequential+block@34c9c30a2bd3486f9e63e18552818286
 
-        (org, num, category, url_name) = mid.rsplit('/',3)
+        if mid.count('+') >= 4:
+            (org, num, run, category, url_name) = mid.rsplit('+',4)
+            if 'problem' in category:
+                category = 'problem'
+        else:
+            (org, num, category, url_name) = mid.rsplit('/',3)
+
         if not category=='problem':     # filter based on category info in module_id
             continue
         try:
