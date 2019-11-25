cea1881630b8c4fa59b149ce141beaec4d77ac1d
Jon Daries <daries@mit.edu>
2019-11-25 08:54:31 -0800

    coercing ofnset into set to remove duplicates

--- edx2bigquery/make_combined_person_course.py
+++ edx2bigquery/make_combined_person_course.py
@@ -70,6 +70,9 @@ def do_combine(course_id_set, project_id, outdir="DATA", nskip=0,
         #if cnt>2:
         #    break
 
+    # remove any duplicates
+    ofnset = set(ofnset)
+    
     org = course_id_set[-1].split('/',1)[0]	# TODO: better way to choose this
 
     ofn = "person_course_%s_%s.csv" % (org, datetime.datetime.now().strftime('%Y-%m-%d-%H%M%S'))
